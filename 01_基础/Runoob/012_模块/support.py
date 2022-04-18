cdd3f45d4925a30fc249419aef5040631213b97c
CyclingPeach <m15955990735@163.com>
2022-04-18 10:49:09 +0800

    main

---
+++ 01_基础/Runoob/012_模块/support.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# Filename:support.py
+
+def print_func(par):
+    print("Hello:" ,par)
---
+++ 01_基础/Runoob/012_模块/test.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# filename:test.py
+
+import support
+
+support.print_func("Runoob")
